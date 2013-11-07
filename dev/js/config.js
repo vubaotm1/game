d5f80835fb2b09489712baabc57675131998ac3d
Jeroen Verfallie <verfalliejeroen@gmail.com>
2013-11-07 18:20:14 +0100

    Config template

--- dev/js/config.js
+++ dev/js/config.js
@@ -1,15 +1,20 @@
 var Config = {
 
+    assetsPath: 'media/',
+    defaultExt: {
+        img: "png"
+    },
+
     base: {
         width: 480,
         height: 320,
-        scale: 1,
+        scale: 20,
         clearColor: '#000000'
+    },
+    height: {
+        abc: 123
     }
 
 };
-
-
-
 
 module.exports = Config;
