786fd94b93a20e475f81243540596e732cf3d393
Jeroen Verfallie <verfalliejeroen@gmail.com>
2013-11-07 18:19:11 +0100

    Basic asset configuration template

---
+++ dev/js/data/media.js
@@ -0,0 +1,25 @@
+var media = {
+    img: {
+        abc: {
+            file: 'test'
+        },
+        def: {
+            file: 'test2',
+            scale: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        },
+        test: {
+            file: 'test'
+        },
+        items: {},
+        tilesets: {},
+        fonts: {},
+        other: {}
+    },
+    sfx: {
+        sounds: {},
+        music: {}
+    },
+    data: {}
+}
+
+module.exports = media;
