dcff1c8f65f2262ddaf82dc772903ec4e79edf9e
Jeroen Verfallie <verfalliejeroen@gmail.com>
2013-11-18 21:29:59 +0100

    dat.GUI for debugging purpose

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,7 +7,15 @@ window.Keys = require('./engine/keys');
 Input.bind('a', [Keys.A]);
 Input.bind('physDebugToggle', [Keys.P]);
 
+
 window.Assets = require('./engine/assets');
+
+
+var dat = require('./lib/dat.gui');
+var gui = new dat.GUI();
+window.t = {}
+//gui.add(t, 'speed', 0, 500);
+
 
 var engine;
 window.addEventListener('load', function() {
@@ -18,5 +26,7 @@ window.addEventListener('resize', function() {
     if(engine) engine.resize.call(engine);
 })
 
-document.addEventListener('keydown', Input.keydown.bind(Input));
-document.addEventListener('keyup', Input.keyup.bind(Input));
+document.addEventListener('keydown', Input._down.bind(Input));
+document.addEventListener('keyup', Input._up.bind(Input));
+document.addEventListener('mousedown', Input._down.bind(Input));
+document.addEventListener('mouseup', Input._up.bind(Input));
