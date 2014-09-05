00ea566fbab6c7f29df7c3ec3ed22cdd3b5c27b2
Jeroen Verfallie <verfalliejeroen@gmail.com>
2014-09-05 19:33:07 +0200

    Added analytics

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,32 +1,11 @@
 require('./lib/class.js');
 require('./lib/requestAnimFrame');
+require('./analytics');
 
 var Input = require('./engine/input');
-var config = require('./config');
-// DEBUG STUFF
-
-// window.Stats = require('./lib/stats');
-
-// window.debug = {
-//     draws: 0,
-//     physdebug: true
-// }
-// var dat = require('./lib/dat.gui');
-// window.gui = new dat.GUI({
-//     autoPlace: true
-// });
-
-// gui.add(debug, 'draws').listen();
-// gui.add(config.physics, 'debug').listen();
-// gui.add(debug, 'physdebug');
-// gui.close();
-
-// GLOBALS
-
-
-
 
 var engine;
+
 $(function() {
     engine = new(require('./engine/engine'))();
 });
@@ -39,11 +18,14 @@ $(document).keydown(Input._down.bind(Input));
 $(document).keyup(Input._up.bind(Input));
 $(document).mousedown(Input._down.bind(Input));
 $(document).mouseup(Input._up.bind(Input));
-$(document).bind("contextmenu", function(e) {
+$(document).bind('contextmenu', function() {
     return false;
 });
 
 
 $('#github').click(function() {
-    window.location = "https://github.com/jeroenverfallie/game-off-2013";
-})
+    window.location = 'https://github.com/jeroenverfallie/game-off-2013';
+});
+
+
+
